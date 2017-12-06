e2fcea12403542a2bbfec30fa11db3759a11f6f5
Dmytro Kotenko <d.kotenko@i.ua>
2017-12-06 19:41:17 +0200

    Added static demonstration

--- samples/oop/burger/Sandwich.php
+++ samples/oop/burger/Sandwich.php
@@ -3,6 +3,11 @@
 class Sandwich
 {
     /**
+     * @var int
+     */
+    private static $counter = 0;
+
+    /**
      * @var Bread
      */
     private $bread;
@@ -18,6 +23,11 @@ class Sandwich
     private $butter;
 
     /**
+     * @var int
+     */
+    private $id;
+
+    /**
      * Sandwich constructor.
      * @param Bread $bread
      * @param Butter $butter
@@ -25,6 +35,9 @@ class Sandwich
      */
     public function __construct(Bread $bread, Butter $butter, Cheese $cheese)
     {
+        self::$counter++;
+        $this->id = self::$counter;
+
         $this->bread = $bread;
         $this->butter = $butter;
         $this->cheese = $cheese;
@@ -39,8 +52,16 @@ class Sandwich
         return "Sandwich with {$bread},<br>{$butter} and<br>{$cheese}<br><br>";
     }
 
+    /**
+     * @return int
+     */
+    public static function getCounter()
+    {
+        return self::$counter;
+    }
+
     public function __destruct()
     {
-        echo spl_object_hash($this), ': Kitchen is closed<br>';
+        echo $this->id, ': Kitchen is closed<br>';
     }
 }
