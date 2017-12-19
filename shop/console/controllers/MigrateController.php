77f286660bd0a54ebd97f143c9b5b0396b572a43
Dmytro Kotenko <d.kotenko@i.ua>
2017-12-19 21:02:29 +0200

    Added migrations home work

--- shop/console/controllers/MigrateController.php
+++ shop/console/controllers/MigrateController.php
@@ -10,6 +10,21 @@ class MigrateController
 {
     public function actionUp()
     {
-        var_dump(123333);
+        # php ./run.php migrate/up
+        # run all new migrations from console/migrations folder
+        # mark completed migrations as done
+    }
+
+    public function actionDown($quantity)
+    {
+        # php ./run.php migrate/down {n}
+        # down {n} completed migrations
+        # unmark down migrations as completed
+    }
+
+    public function actionCreate($migrationName)
+    {
+        # php ./run.php migrate/create create_users_table
+        # creating new class like "{time}_create_users_table" in console/migrations folder
     }
 }
