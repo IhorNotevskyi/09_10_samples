d7374e2d7fc116a48744701f70c090337d6c8dfd
Dmytro Kotenko <d.kotenko@i.ua>
2018-03-05 23:57:38 +0200

    Added pdf package

--- samples/office/index.php
+++ samples/office/index.php
@@ -2,8 +2,16 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$pdf = new \Knp\Snappy\Pdf('/usr/local/bin/wkhtmltopdf-amd64');
+use app\PDF;
 
-//header('Content-Type: application/pdf');
-//header('Content-Disposition: attachment; filename="file.pdf"');
-echo $pdf->getOutput('http://mignews.com.ua/');
+$builder = (new PDF())->getBuilder();
+
+$builder->AddPage();
+$builder->Image('https://php-academy.kiev.ua/2017/img/logo-footer.png');
+$builder->SetFont(PDF::ARIAL_FAMILY, PDF::BOLD_TEXT, 24);
+$builder->Ln(50);
+
+$text = 'Получите 6 месяцев курсов FullStack с IT English + трудоустройство с испытательным сроком в 3 месяца. Минимальная зарплата на время испытательного срока - от $250 до $350 в месяц. Контракт на стажировку подписывается при старте курсов. ';
+$builder->Cell(100, 20, $text);
+
+$builder->Output("report.pdf", PDF::BROWSER_OUTPUT);
