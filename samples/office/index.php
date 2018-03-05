5487142805646ffb2ce8c1d28df14e32e24dcdd9
Dmytro Kotenko <d.kotenko@i.ua>
2018-03-06 00:02:00 +0200

    Added pdf package

--- samples/office/index.php
+++ samples/office/index.php
@@ -8,10 +8,10 @@ $builder = (new PDF())->getBuilder();
 
 $builder->AddPage();
 $builder->Image('https://php-academy.kiev.ua/2017/img/logo-footer.png');
-$builder->SetFont(PDF::ARIAL_FAMILY, PDF::BOLD_TEXT, 24);
-$builder->Ln(50);
+$builder->SetFont(PDF::ARIAL_FAMILY, PDF::BOLD_TEXT, 14);
+$builder->Ln(10);
 
-$text = 'Получите 6 месяцев курсов FullStack с IT English + трудоустройство с испытательным сроком в 3 месяца. Минимальная зарплата на время испытательного срока - от $250 до $350 в месяц. Контракт на стажировку подписывается при старте курсов. ';
+$text = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.';
 $builder->Cell(100, 20, $text);
 
 $builder->Output("report.pdf", PDF::BROWSER_OUTPUT);
